b50ed41ec53f19184d8c8c362308a944784d544c
Stefan Majewsky <stefan.majewsky@sap.com>
2017-10-16 11:38:35 +0200

    add test for segmenting support

--- tests.sh
+++ tests.sh
@@ -264,6 +264,51 @@ Hello Another World.
 EOF
 
 ################################################################################
+step 'Test 6: Segmenting of large files'
+
+upload_file_from_stdin largefile.txt <<-EOF
+  Line number 1
+  Line number 2
+  Line number 3
+  Line number 4
+  Line number 5
+EOF
+
+mirror <<-EOF
+  swift: { $AUTH_PARAMS }
+  jobs:
+    - from: ${SOURCE_SPEC}
+      to: { container: ${CONTAINER_BASE}-test6 }
+      segmenting:
+        container: ${CONTAINER_BASE}-test6-segments
+        min_bytes: 30
+        segment_bytes: 14
+EOF
+# NOTE: A segment size of 14 bytes should put each line of text in its own
+# segment, i.e. 5 segments.
+
+expect test6 <<-EOF
+>> just/another/file.txt
+This is the new file!
+>> just/some/files/1.txt
+Hello World.
+>> just/some/files/2.txt
+Hello Second World.
+>> largefile.txt
+Line number 1
+Line number 2
+Line number 3
+Line number 4
+Line number 5
+EOF
+
+SEGMENT_COUNT="$(swift list ${CONTAINER_BASE}-test6-segments | wc -l)"
+if [ "${SEGMENT_COUNT}" -ne 5 ]; then
+  echo -e "\e[1;31m>>\e[0;31m Expected SLO to have 5 segments, but got ${SEGMENT_COUNT} instead:\e[0m"
+  swift list ${CONTAINER_BASE}-test6-segments | sed 's/^/    /'
+fi
+
+################################################################################
 # cleanup before exiting
 
 cleanup_containers
