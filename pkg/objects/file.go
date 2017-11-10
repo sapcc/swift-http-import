269ebd6ab925f45be2271ab75310b7127982d8ca
Falk Reimann <falk.reimann@sap.com>
2017-11-10 21:15:36 +0100

    Don't delete failed upload caused by rate limit
    If the upload failed due to rate limit (HTTP 498), there is no need to delete
    the failed object, because it will definitely not in Swift. The delete itself
    might cause an additional rate limit response.
    This fix only applies to non large object uplaods.

--- pkg/objects/file.go
+++ pkg/objects/file.go
@@ -157,10 +157,16 @@ func (f File) uploadNormalObject(body io.Reader, sourceState FileState, hdr swif
 
 	util.Log(util.LogError, "PUT %s/%s failed: %s", containerName, objectName, err.Error())
 
-	//delete potentially incomplete upload
-	err = f.Job.Target.Connection.ObjectDelete(containerName, objectName)
-	if err != nil {
-		util.Log(util.LogError, "DELETE %s/%s failed: %s", containerName, objectName, err.Error())
+	if serr, ok := err.(*swift.Error); ok {
+		//upload failed due to rate limit, object is definitely not uploaded
+		//prevent additional rate limit caused by an unnecessary delete request
+		if serr.StatusCode != 498 {
+			//delete potentially incomplete upload
+			err = f.Job.Target.Connection.ObjectDelete(containerName, objectName)
+			if err != nil {
+				util.Log(util.LogError, "DELETE %s/%s failed: %s", containerName, objectName, err.Error())
+			}
+		}
 	}
 
 	return false
