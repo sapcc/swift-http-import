ba214000a4f92bbf971042e8608337976a8eb043
kayrus <kay.diam@gmail.com>
2019-11-28 18:45:17 +0100

    Add application credential auth support

--- make-config.sh
+++ make-config.sh
@@ -16,13 +16,16 @@ fi
 
 sed '/\(region_name\|object_prefix\):\s*$/d' <<-EOF
 swift:
-  auth_url:            ${OS_AUTH_URL}
-  user_name:           ${OS_USERNAME}
-  user_domain_name:    ${OS_USER_DOMAIN_NAME}
-  project_name:        ${OS_PROJECT_NAME}
-  project_domain_name: ${OS_PROJECT_DOMAIN_NAME}
-  password:            ${OS_PASSWORD}
-  region_name:         ${OS_REGION_NAME}
+  auth_url:                      ${OS_AUTH_URL}
+  user_name:                     ${OS_USERNAME}
+  user_domain_name:              ${OS_USER_DOMAIN_NAME}
+  project_name:                  ${OS_PROJECT_NAME}
+  project_domain_name:           ${OS_PROJECT_DOMAIN_NAME}
+  password:                      ${OS_PASSWORD}
+  region_name:                   ${OS_REGION_NAME}
+  application_credential_id:     ${OS_APPLICATION_CREDENTIAL_ID}
+  application_credential_name:   ${OS_APPLICATION_CREDENTIAL_NAME}
+  application_credential_secret: ${OS_APPLICATION_CREDENTIAL_SECRET}
 
 workers:
   transfer: 1
