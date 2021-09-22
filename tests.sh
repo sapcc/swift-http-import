7882338f70bd7149dab7c2ba3fe7cff5b5ec411e
Sandro Jäckel <sandro.jaeckel@sap.com>
2021-09-22 15:19:31 +0200

    Format with `shfmt -ci -i 2 -s -w tests.sh`

--- tests.sh
+++ tests.sh
@@ -67,7 +67,7 @@ step 'Preparing source container'
 upload_file_from_stdin() {
   # `swift upload` is stupid; it will stubbornly refuse any pipes or FIFOs and
   # only accept plain regular files, so I have to use a temp file here
-  sed 's/^  //' > "${TEST_FILENAME}"
+  sed 's/^  //' >"${TEST_FILENAME}"
   OBJECT_NAME="$1"
   shift
   swift upload "${CONTAINER_PUBLIC}" "${TEST_FILENAME}" --object-name "${DISAMBIGUATOR}/${OBJECT_NAME}" "$@"
@@ -97,7 +97,7 @@ fi
 upload_target_file_from_stdin() {
   # same trickery as in `upload_file_from_stdin` (see comments over there), but
   # for a target container
-  sed 's/^  //' > "${TEST_FILENAME}"
+  sed 's/^  //' >"${TEST_FILENAME}"
   local CONTAINER_NAME="${CONTAINER_BASE}-$1"
   local OBJECT_NAME="$2"
   shift 2
@@ -123,7 +123,7 @@ dump() {
 expect() {
   local ACTUAL="$(dump "$1")"
   local EXPECTED="$(cat)"
-  if ! diff -q <(echo "${EXPECTED}") <(echo "${ACTUAL}") > /dev/null; then
+  if ! diff -q <(echo "${EXPECTED}") <(echo "${ACTUAL}") >/dev/null; then
     printf "\e[1;31m>>\e[0;31m Contents of target container ${CONTAINER_BASE}-$1 do not match expectation. Diff follows:\e[0m\n"
     diff -u <(echo "${EXPECTED}") <(echo "${ACTUAL}")
   fi
@@ -381,7 +381,7 @@ if [ "$1" = http ]; then
   echo ">> Test skipped (works only with Swift source)."
 else
 
-mirror <<-EOF
+  mirror <<-EOF
   swift: { $AUTH_PARAMS }
   jobs:
     - from: ${SOURCE_SPEC}
@@ -395,7 +395,7 @@ mirror <<-EOF
         segment_bytes: 30
 EOF
 
-expect test7 <<-EOF
+  expect test7 <<-EOF
 >> expires-with-segments.txt
 This will expire soon.
 This will expire soon.
@@ -403,25 +403,25 @@ This will expire soon.
 This will expire soon.
 EOF
 
-for OBJECT_NAME in expires.txt expires-with-segments.txt; do
-  EXPIRY_TIMESTAMP="$(swift stat ${CONTAINER_BASE}-test7 ${OBJECT_NAME} | awk '/X-Delete-At:/ { print $2 }')"
-  if [ "${EXPIRY_TIMESTAMP}" != 2000000042 ]; then
-    printf "\e[1;31m>>\e[0;31m Expected file \"${OBJECT_NAME}\" to expire at timestamp 2000000042, but expires at timestamp '${EXPIRY_TIMESTAMP}' instead.\e[0m\n"
-    exit 1
-  fi
-done
+  for OBJECT_NAME in expires.txt expires-with-segments.txt; do
+    EXPIRY_TIMESTAMP="$(swift stat ${CONTAINER_BASE}-test7 ${OBJECT_NAME} | awk '/X-Delete-At:/ { print $2 }')"
+    if [ "${EXPIRY_TIMESTAMP}" != 2000000042 ]; then
+      printf "\e[1;31m>>\e[0;31m Expected file \"${OBJECT_NAME}\" to expire at timestamp 2000000042, but expires at timestamp '${EXPIRY_TIMESTAMP}' instead.\e[0m\n"
+      exit 1
+    fi
+  done
 
-# also check that expiration dates are applied to the segments as well
-swift list ${CONTAINER_BASE}-test7-segments | while read OBJECT_NAME; do
-  EXPIRY_TIMESTAMP="$(swift stat ${CONTAINER_BASE}-test7-segments ${OBJECT_NAME} | awk '/X-Delete-At:/ { print $2 }')"
-  if [ "${EXPIRY_TIMESTAMP}" != 2000000042 ]; then
-    printf "\e[1;31m>>\e[0;31m Expected segment '${OBJECT_NAME}' to expire at timestamp 2000000042, but expires at timestamp '${EXPIRY_TIMESTAMP}' instead.\e[0m\n"
+  # also check that expiration dates are applied to the segments as well
+  swift list ${CONTAINER_BASE}-test7-segments | while read OBJECT_NAME; do
+    EXPIRY_TIMESTAMP="$(swift stat ${CONTAINER_BASE}-test7-segments ${OBJECT_NAME} | awk '/X-Delete-At:/ { print $2 }')"
+    if [ "${EXPIRY_TIMESTAMP}" != 2000000042 ]; then
+      printf "\e[1;31m>>\e[0;31m Expected segment '${OBJECT_NAME}' to expire at timestamp 2000000042, but expires at timestamp '${EXPIRY_TIMESTAMP}' instead.\e[0m\n"
+      exit 1
+    fi
+  done || (
+    printf "\e[1;31m>>\e[0;31m Expected object 'expires-with-segments.txt' to be an SLO, but it's not segmented.\e[0m\n"
     exit 1
-  fi
-done || (
-  printf "\e[1;31m>>\e[0;31m Expected object 'expires-with-segments.txt' to be an SLO, but it's not segmented.\e[0m\n"
-  exit 1
-)
+  )
 
 fi # end of: if [ "$1" = http ]
 
@@ -437,7 +437,7 @@ if [ "$1" = swift ]; then
   echo ">> Test skipped (works only with HTTP source)."
 else
 
-mirror <<-EOF
+  mirror <<-EOF
   swift: { $AUTH_PARAMS }
   jobs:
     - from:
@@ -451,10 +451,10 @@ mirror <<-EOF
         min_bytes: 30
         segment_bytes: 14
 EOF
-# NOTE: A segment size of 14 bytes should put each line of text in its own
-# segment, i.e. 5 segments.
+  # NOTE: A segment size of 14 bytes should put each line of text in its own
+  # segment, i.e. 5 segments.
 
-expect test8 <<-EOF
+  expect test8 <<-EOF
 >> expires.txt
 This will expire soon.
 >> just/another/file.txt
@@ -471,12 +471,12 @@ Line number 4
 CHANGED
 EOF
 
-SEGMENT_COUNT="$(swift list ${CONTAINER_BASE}-test8-segments | wc -l)"
-if [ "${SEGMENT_COUNT}" -ne 5 ]; then
-  printf "\e[1;31m>>\e[0;31m Expected SLO to have 5 segments, but got ${SEGMENT_COUNT} instead:\e[0m\n"
-  dump test8-segments
-  exit 1
-fi
+  SEGMENT_COUNT="$(swift list ${CONTAINER_BASE}-test8-segments | wc -l)"
+  if [ "${SEGMENT_COUNT}" -ne 5 ]; then
+    printf "\e[1;31m>>\e[0;31m Expected SLO to have 5 segments, but got ${SEGMENT_COUNT} instead:\e[0m\n"
+    dump test8-segments
+    exit 1
+  fi
 
 fi # end of: if [ "$1" = swift ]
 
@@ -487,11 +487,11 @@ if [ "$1" = http ]; then
   echo ">> Test skipped (works only with Swift source)."
 else
 
-# Uploading a symlink requires curl because python-swiftclient has not catched up with Swift yet.
-curl -H "X-Auth-Token: ${OS_AUTH_TOKEN}" -X PUT -d '' -H "Content-Type: application/symlink" -H "X-Symlink-Target: swift-http-import-source/${DISAMBIGUATOR}/just/some/files/1.txt" "${SOURCE_URL}/just/a/symlink.txt"
-sleep 10 # wait for container listing to get updated
+  # Uploading a symlink requires curl because python-swiftclient has not catched up with Swift yet.
+  curl -H "X-Auth-Token: ${OS_AUTH_TOKEN}" -X PUT -d '' -H "Content-Type: application/symlink" -H "X-Symlink-Target: swift-http-import-source/${DISAMBIGUATOR}/just/some/files/1.txt" "${SOURCE_URL}/just/a/symlink.txt"
+  sleep 10 # wait for container listing to get updated
 
-mirror <<-EOF
+  mirror <<-EOF
   swift: { $AUTH_PARAMS }
   jobs:
     - from: ${SOURCE_SPEC}
@@ -506,7 +506,7 @@ mirror <<-EOF
         object_prefix: symlink-and-target
 EOF
 
-expect test9 <<-EOF
+  expect test9 <<-EOF
 >> only-symlink/just/a/symlink.txt
 Hello World.
 >> symlink-and-target/just/a/symlink.txt
@@ -517,21 +517,21 @@ Hello World.
 Hello Second World.
 EOF
 
-# check that the "only-symlink" job transfers symlink.txt as a regular file (it cannot
-# transfer as a symlink because the link target is missing on the target side)
-if curl -si -H "X-Auth-Token: ${OS_AUTH_TOKEN}" "${OS_STORAGE_URL}/${CONTAINER_BASE}-test9/only-symlink/just/a/symlink.txt?symlink=get" | grep -qi '^X-Symlink-Target'; then
-  printf "\e[1;31m>>\e[0;31m Expected only-symlink/just/a/symlink.txt not to be a symlink, but it is one:\e[0m\n"
-  curl -si -H "X-Auth-Token: ${OS_AUTH_TOKEN}" "${OS_STORAGE_URL}/${CONTAINER_BASE}-test9/only-symlink/just/a/symlink.txt?symlink=get"
-  exit 1
-fi
+  # check that the "only-symlink" job transfers symlink.txt as a regular file (it cannot
+  # transfer as a symlink because the link target is missing on the target side)
+  if curl -si -H "X-Auth-Token: ${OS_AUTH_TOKEN}" "${OS_STORAGE_URL}/${CONTAINER_BASE}-test9/only-symlink/just/a/symlink.txt?symlink=get" | grep -qi '^X-Symlink-Target'; then
+    printf "\e[1;31m>>\e[0;31m Expected only-symlink/just/a/symlink.txt not to be a symlink, but it is one:\e[0m\n"
+    curl -si -H "X-Auth-Token: ${OS_AUTH_TOKEN}" "${OS_STORAGE_URL}/${CONTAINER_BASE}-test9/only-symlink/just/a/symlink.txt?symlink=get"
+    exit 1
+  fi
 
-# check that the "symlink-and-target" job transfers symlink.txt as a symlink
-# (since its link target is also included in the job)
-if ! curl -si -H "X-Auth-Token: ${OS_AUTH_TOKEN}" "${OS_STORAGE_URL}/${CONTAINER_BASE}-test9/symlink-and-target/just/a/symlink.txt?symlink=get" | grep -qi '^X-Symlink-Target'; then
-  printf "\e[1;31m>>\e[0;31m Expected symlink-and-target/just/a/symlink.txt to be a symlink, but it is not:\e[0m\n"
-  curl -si -H "X-Auth-Token: ${OS_AUTH_TOKEN}" "${OS_STORAGE_URL}/${CONTAINER_BASE}-test9/symlink-and-target/just/a/symlink.txt?symlink=get"
-  exit 1
-fi
+  # check that the "symlink-and-target" job transfers symlink.txt as a symlink
+  # (since its link target is also included in the job)
+  if ! curl -si -H "X-Auth-Token: ${OS_AUTH_TOKEN}" "${OS_STORAGE_URL}/${CONTAINER_BASE}-test9/symlink-and-target/just/a/symlink.txt?symlink=get" | grep -qi '^X-Symlink-Target'; then
+    printf "\e[1;31m>>\e[0;31m Expected symlink-and-target/just/a/symlink.txt to be a symlink, but it is not:\e[0m\n"
+    curl -si -H "X-Auth-Token: ${OS_AUTH_TOKEN}" "${OS_STORAGE_URL}/${CONTAINER_BASE}-test9/symlink-and-target/just/a/symlink.txt?symlink=get"
+    exit 1
+  fi
 
 fi # end of: if [ "$1" = http ]
 
@@ -600,12 +600,12 @@ if [ "$1" = http ]; then
   echo ">> Test skipped (works only with Swift source)."
 else
 
-upload_file_from_stdin pseudo/directory/ < /dev/null
-upload_file_from_stdin pseudo/regularfile.txt <<-EOF
+  upload_file_from_stdin pseudo/directory/ </dev/null
+  upload_file_from_stdin pseudo/regularfile.txt <<-EOF
   Hello File.
 EOF
 
-mirror <<-EOF
+  mirror <<-EOF
   swift: { $AUTH_PARAMS }
   jobs:
     - from: ${SOURCE_SPEC}
@@ -614,7 +614,7 @@ mirror <<-EOF
       only: pseudo/
 EOF
 
-expect test11 <<-EOF
+  expect test11 <<-EOF
 >> pseudo/directory/
 >> pseudo/regularfile.txt
 Hello File.
@@ -629,12 +629,12 @@ if [ "$1" = http ]; then
   echo ">> Test skipped (works only with Swift source)."
 else
 
-# reset Last-Modified timestamp on this one file
-upload_file_from_stdin just/some/files/2.txt <<-EOF
+  # reset Last-Modified timestamp on this one file
+  upload_file_from_stdin just/some/files/2.txt <<-EOF
   Hello Second World.
 EOF
 
-mirror <<-EOF
+  mirror <<-EOF
   swift: { $AUTH_PARAMS }
   jobs:
     - from: ${SOURCE_SPEC}
@@ -645,7 +645,7 @@ mirror <<-EOF
         not_older_than: 30 seconds
 EOF
 
-expect test12 <<-EOF
+  expect test12 <<-EOF
 >> just/some/files/2.txt
 Hello Second World.
 EOF
@@ -659,37 +659,37 @@ if ! hash rclone &>/dev/null; then
   echo ">> Test skipped (rclone is not installed, instructions: https://rclone.org/install/)."
 else
 
-rclone_cmd() {
-  RCLONE_CONFIG_TESTREMOTE_TYPE=swift RCLONE_CONFIG_TESTREMOTE_ENV_AUTH=1 rclone "$@"
-}
+  rclone_cmd() {
+    RCLONE_CONFIG_TESTREMOTE_TYPE=swift RCLONE_CONFIG_TESTREMOTE_ENV_AUTH=1 rclone "$@"
+  }
 
-upload_test_file_using_rclone() {
-  local file_name="$1"
-  echo "This is a test file." > "${file_name}"
-  rclone_cmd copy "${file_name}" TESTREMOTE:"${CONTAINER_BASE}/from"
-  rclone_cmd copy "${file_name}" TESTREMOTE:"${CONTAINER_BASE}/to"
-}
+  upload_test_file_using_rclone() {
+    local file_name="$1"
+    echo "This is a test file." >"${file_name}"
+    rclone_cmd copy "${file_name}" TESTREMOTE:"${CONTAINER_BASE}/from"
+    rclone_cmd copy "${file_name}" TESTREMOTE:"${CONTAINER_BASE}/to"
+  }
 
-if hash gdate &>/dev/null; then
-  date() { gdate "$@"; }
-fi
+  if hash gdate &>/dev/null; then
+    date() { gdate "$@"; }
+  fi
 
-get_swift_object_mtime() {
-  date -d "$(swift stat ${CONTAINER_BASE} $1 |
-    grep 'Last Modified:' |
-    sed -E 's/Last Modified:\s*(.*)/\1/')" '+%s'
-}
+  get_swift_object_mtime() {
+    date -d "$(swift stat ${CONTAINER_BASE} $1 |
+      grep 'Last Modified:' |
+      sed -E 's/Last Modified:\s*(.*)/\1/')" '+%s'
+  }
 
-# upload test files using rclone and get their mtime
-upload_test_file_using_rclone "${TEST_DIR}/rclone-test-file-1"
-upload_test_file_using_rclone "${TEST_DIR}/rclone-test-file-2"
-sleep 10 # wait for container listing to get updated
+  # upload test files using rclone and get their mtime
+  upload_test_file_using_rclone "${TEST_DIR}/rclone-test-file-1"
+  upload_test_file_using_rclone "${TEST_DIR}/rclone-test-file-2"
+  sleep 10 # wait for container listing to get updated
 
-before_mtime_1="$(get_swift_object_mtime to/rclone-test-file-1)"
-before_mtime_2="$(get_swift_object_mtime to/rclone-test-file-2)"
+  before_mtime_1="$(get_swift_object_mtime to/rclone-test-file-1)"
+  before_mtime_2="$(get_swift_object_mtime to/rclone-test-file-2)"
 
-# mirror test files using swift-http-import and compare the mtime
-mirror <<-EOF
+  # mirror test files using swift-http-import and compare the mtime
+  mirror <<-EOF
   swift: { $AUTH_PARAMS }
   jobs:
     - from: { container: ${CONTAINER_BASE}, object_prefix: from, ${AUTH_PARAMS} }
@@ -700,13 +700,13 @@ mirror <<-EOF
         simplistic_comparison: true
 EOF
 
-after_mtime_1="$(get_swift_object_mtime to/rclone-test-file-1)"
-after_mtime_2="$(get_swift_object_mtime to/rclone-test-file-2)"
+  after_mtime_1="$(get_swift_object_mtime to/rclone-test-file-1)"
+  after_mtime_2="$(get_swift_object_mtime to/rclone-test-file-2)"
 
-if ! [ "$before_mtime_1" == "$after_mtime_1" ]  || ! [ "$before_mtime_2" == "$after_mtime_2" ]; then
-  printf "\e[1;31m>>\e[0;31m Files in ${CONTAINER_BASE} have been modified by swift-http-import. They were expected not to be modified.\e[0m\n"
-  exit 1
-fi
+  if ! [ "$before_mtime_1" == "$after_mtime_1" ] || ! [ "$before_mtime_2" == "$after_mtime_2" ]; then
+    printf "\e[1;31m>>\e[0;31m Files in ${CONTAINER_BASE} have been modified by swift-http-import. They were expected not to be modified.\e[0m\n"
+    exit 1
+  fi
 
 fi # end of: if ! hash rclone &>/dev/null
 
