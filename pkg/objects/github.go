6a8c0aca4cf862ded4605ab7dd29cc984ea10a07
Stefan Majewsky <stefan.majewsky@sap.com>
2025-03-21 16:13:18 +0100

    bump go-github/github from v69 to v70
    Renovate cannot figure this out by itself.

--- pkg/objects/github.go
+++ pkg/objects/github.go
@@ -29,7 +29,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/google/go-github/v69/github"
+	"github.com/google/go-github/v70/github"
 	"github.com/majewsky/schwift/v2"
 	"github.com/sapcc/go-api-declarations/bininfo"
 	"github.com/sapcc/go-bits/secrets"
